4cef94150e1835c20229ed02619b665ef4c99b6b
dualkk1997 <dualkk1997@gmail.com>
2022-04-14 16:33:56 +0800

    Update heroku

--- FUJI/src/main/java/tw/timhsu/FujiApplication.java
+++ FUJI/src/main/java/tw/timhsu/FujiApplication.java
@@ -2,7 +2,8 @@ package tw.timhsu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class FujiApplication {
 
